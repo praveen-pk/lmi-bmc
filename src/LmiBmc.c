a95e96173c9eb25181f3f9671fd125530a97262e
Praveen K Paladugu <praveen_paladugu@dell.com>
2014-05-07 17:40:34 -0500

    Some Licensing info. Added a custom source to capture provider specific functions

---
+++ src/LmiBmc.c
@@ -0,0 +1,12 @@
+/*
+ *  Copyright (c) 2014 Dell, Inc. All rights reserved.
+ *  Licensed under the GNU General Public license, version 2.1 or any later version.
+ *  Authors: Praveen K Paladugu <praveen_paladugu@dell.com>
+ */
+#include "LmiBmc.h"
+#include <stdio.h>
+
+void test()
+{
+    printf("Just a test function");
+}
