a9a44c99bd3a334aa1d64e3eeb97b39732e88992
Praveen K Paladugu <praveen_paladugu@dell.com>
2014-05-13 14:15:44 -0500

    Added functions to check BIOS Vendor

--- src/LmiBmc.c
+++ src/LmiBmc.c
@@ -4,9 +4,315 @@
  *  Authors: Praveen K Paladugu <praveen_paladugu@dell.com>
  */
 #include "LmiBmc.h"
-#include <stdio.h>
 
-void test()
+
+/*From openlmi-providers/src/hardware/utils.c
+ * */
+char *trim(const char *str, const char *delims)
 {
-    printf("Just a test function");
+    char *out;
+    const char *default_delims = WHITESPACES;
+    size_t l;
+
+    /* if string is empty */
+    if (!str || strlen(str) < 1) {
+        return NULL;
+    }
+
+    if (!delims) {
+        delims = default_delims;
+    }
+
+    /* trim beginning of the string */
+    while (strchr(delims, str[0]) && str[0] != '\0') {
+        str++;
+    }
+
+    l = strlen(str);
+
+    /* if string was only white spaces */
+    if (l < 1) {
+        return NULL;
+    }
+
+    /* shorten length of string if there are trailing white spaces */
+    while (strchr(delims, str[l - 1]) && l > 0) {
+        l--;
+    }
+
+    /* sanity check */
+    if (l < 1) {
+        return NULL;
+    }
+
+    /* copy string */
+    out = strndup(str, l);
+    if (!out) {
+        lmi_warn("Failed to allocate memory.");
+    }
+
+    return out;
 }
+
+
+/*From openlmi-providers/src/hardware/utils.c
+ * */
+short read_fp_to_2d_buffer(FILE *fp, char ***buffer, unsigned *buffer_size)
+{
+    short ret = -1;
+    ssize_t read;
+    size_t line_len = 0;
+    unsigned tmp_buffer_lines = 0, lines_read = 0;
+    char **tmp_buffer = NULL, *line = NULL;
+
+    free_2d_buffer(buffer, buffer_size);
+
+    if (!fp) {
+        lmi_warn("Given file pointer is NULL.");
+        goto done;
+    }
+
+    /* allocate buffer */
+    tmp_buffer_lines = 128;
+    tmp_buffer = (char **)calloc(tmp_buffer_lines, sizeof(char *));
+    if (!tmp_buffer) {
+        lmi_warn("Failed to allocate memory.");
+        tmp_buffer_lines = 0;
+        goto done;
+    }
+
+    while ((read = getline(&line, &line_len, fp)) != -1) {
+        /* filter comment lines */
+        if (read > 0 && line[0] == '#') {
+            continue;
+        }
+
+        /* reallocate if needed */
+        if (lines_read >= tmp_buffer_lines) {
+            tmp_buffer_lines *= 2;
+            char **newtmp = (char **)realloc(tmp_buffer,
+                    tmp_buffer_lines * sizeof(char *));
+            if (!newtmp) {
+                lmi_warn("Failed to allocate memory.");
+                tmp_buffer_lines /= 2;
+                goto done;
+            }
+            tmp_buffer = newtmp;
+        }
+
+        /* copy trimmed line to buffer */
+        tmp_buffer[lines_read] = trim(line,NULL );
+        if (!tmp_buffer[lines_read]) {
+            tmp_buffer[lines_read] = strdup("");
+            if (!tmp_buffer[lines_read]) {
+                lmi_warn("Failed to allocate memory.");
+                goto done;
+            }
+        }
+        lines_read++;
+    }
+
+    if (lines_read < 1) {
+        lmi_warn("No data read from given source.");
+        goto done;
+    }
+
+    /* reallocate buffer to free unused space */
+    if (tmp_buffer_lines > lines_read) {
+        char **newtmp = (char **)realloc(tmp_buffer,
+                lines_read * sizeof(char *));
+        if (!newtmp) {
+            lmi_warn("Failed to allocate memory.");
+            goto done;
+        }
+        tmp_buffer = newtmp;
+        tmp_buffer_lines = lines_read;
+    }
+
+    *buffer_size = tmp_buffer_lines;
+    *buffer = tmp_buffer;
+
+    ret = 0;
+
+done:
+    free(line);
+
+    if (ret != 0) {
+        free_2d_buffer(&tmp_buffer, &tmp_buffer_lines);
+    }
+
+    return ret;
+}
+
+/*From openlmi-providers/src/hardware/utils.c
+ * */
+void free_2d_buffer(char ***buffer, unsigned *buffer_size)
+{
+    unsigned i, tmp_buffer_lines = *buffer_size;
+    char **tmp_buffer = *buffer;
+
+    if (tmp_buffer && tmp_buffer_lines > 0) {
+        for (i = 0; i < tmp_buffer_lines; i++) {
+            free(tmp_buffer[i]);
+            tmp_buffer[i] = NULL;
+        }
+        free(tmp_buffer);
+    }
+
+    tmp_buffer = NULL;
+    *buffer_size = 0;
+    *buffer = NULL;
+}
+
+/*
+char * get_manufacturer ()
+{
+    int buf_len = BUFLEN, start_index, line_len, read;
+    int status;
+    char *start,*line, *vendor;
+    FILE *fp;
+    
+    vendor = calloc(buf_len, sizeof(char) );
+    lmi_debug ("Running 'dmidecode -t 1| grep Manufacturer ' command\n");
+    fp = popen("sudo dmidecode -t bios | grep Vendor", "r");
+    if (!fp){
+	lmi_error ("Failed to run dmidecode command. Exiting.");
+    }
+    read = getline(&line, &line_len, fp);
+    if (read == -1){
+	lmi_error ("Failed while reading the output from dmidecode");
+    }
+    status = pclose (fp);
+    if (status == -1){
+	lmi_error ("Dmidecode command Failed.\n");
+	printf ("%s",strerror(errno));
+    }
+
+    
+    start = strchr(line, ':');
+    start++;
+
+
+    if (start == NULL){
+	lmi_error ("Didn't find the expected delimiter in dmidecode output");
+    }
+    
+    start_index = start - line; 
+
+	
+    while ( (line_len - start_index) > buf_len )
+    {
+	vendor = realloc(vendor, buf_len*2);
+	buf_len *= 2;
+    }
+    vendor = trim (line + start_index, NULL );
+    printf ("\n%s\n", vendor);
+    return vendor;
+
+}
+*/
+
+/*From openlmi-providers/src/hardware/utils.c
+ * */
+short run_command(const char *command, char ***buffer, unsigned *buffer_size)
+{
+    FILE *fp = NULL;
+    short ret = -1;
+    char errbuf[BUFLEN];
+
+    /* if command is empty */
+    if (!command || strlen(command) < 1) {
+        lmi_warn("Given command is empty.");
+        goto done;
+    }
+
+    /* execute command */
+    lmi_debug("Running command: \"%s\"\n", command);
+    fp = popen(command, "r");
+    if (!fp) {
+        lmi_warn("Failed to run command: \"%s\"; Error: %s",
+                command, strerror_r(errno, errbuf, sizeof(errbuf)));
+        goto done;
+    }
+
+    if (read_fp_to_2d_buffer(fp, buffer, buffer_size) != 0) {
+        goto done;
+    }
+
+    ret = 0;
+
+done:
+    if (fp) {
+        int ret_code = pclose(fp);
+        if (ret_code == -1) {
+            lmi_warn("Failed to run command: \"%s\"; Error: %s",
+                    command, strerror_r(errno, errbuf, sizeof(errbuf)));
+            if (ret == 0) {
+                ret = -1;
+            }
+        } else if (ret_code != 0) {
+            lmi_warn("Command \"%s\" exited unexpectedly with return code: %d",
+                    command, ret_code);
+            ret = ret_code;
+        }
+    }
+
+    if (ret < 0) {
+        free_2d_buffer(buffer, buffer_size);
+    }
+
+    return ret;
+}
+
+static char * get_bios_vendor()
+{
+    int i;
+    int buffer_size =0;
+    char **buffer = NULL, *buf = NULL;
+    char *vendor= NULL;
+
+    if (run_command("sudo dmidecode -t 0| grep Vendor", &buffer, &buffer_size) != 0) {
+        printf ( "Failed running the dmidecode command \n");
+	return "";
+    }
+     
+    if (buffer_size > 1) {
+	lmi_error ("Unexpected output from dmidecode");
+	return "";
+    }
+    vendor = strdup ( strchr(buffer[0],':') +1 );
+    vendor = trim (vendor,"\\. ");
+
+    return vendor;
+}
+
+bool is_vendor_like_dell(char *vendor)
+{
+    
+    const char delim[2]=",";
+    char *tmp_ven;
+    char *vendor_list = strdup (DELL_LIKE_VENDORS);
+    tmp_ven = strtok (vendor_list, delim );
+    tmp_ven = trim (tmp_ven,NULL); 
+    while ( tmp_ven != NULL)
+    {
+	if (strcmp (tmp_ven, vendor) == 0 ){
+	    return true;
+	}
+
+	tmp_ven = strtok(NULL,delim);
+	tmp_ven = trim (tmp_ven,NULL); 
+	
+    }
+    return false;
+     
+}
+
+
+main ()
+{
+    char *vendor=NULL;
+  
+}
+
+
--- src/LmiBmc.h
+++ src/LmiBmc.h
@@ -1,2 +1,79 @@
+/*
+ *  Copyright (c) 2014 Dell, Inc. All rights reserved.
+ *  Licensed under the GNU General Public license, version 2.1 or any later version.
+ *  Authors: Praveen K Paladugu <praveen_paladugu@dell.com>
+ */
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <malloc.h>
+#include <system.h>
 
-void test();
+
+#define DELL_LIKE_VENDORS "Dell Inc, Dell"
+#define str(s) #s
+#define BUFLEN 1024
+#define WHITESPACES " \f\n\r\t\v"
+#define IPv4_Add_Size 11
+
+#define  lmi_warn(...)  printf("\nWARNING:");  printf(__VA_ARGS__)
+#define  lmi_debug(...)  printf("\nDEBUG:");  printf(__VA_ARGS__)
+#define  lmi_error(...)  printf("\nERROR:");  printf(__VA_ARGS__)
+
+
+/*
+ * Run given command and store its output in buffer. Number of lines in buffer
+ * is stored in buffer_size. Function skips lines starting with '#'.
+ * Buffer has to be NULL, and buffer_size 0. Function will allocate necessary
+ * memory, buffer can be freed with free_2d_buffer() function.
+ * @param command to be run
+ * @param buffer
+ * @param buffer_size number of lines in buffer
+ * @return negative value if problem occurred. In this case, buffer is freed.
+ *          0 and positive value represent return code of command.
+ */
+short run_command(const char *command, char ***buffer, unsigned *buffer_size);
+
+/*
+ * Free 2D buffer.
+ * @param buffer
+ * @param buffer_size number of lines in buffer
+ */
+void free_2d_buffer(char ***buffer, unsigned *buffer_size);
+
+
+
+/*
+ * Create trimmed copy of given string. Trimmed will be any characters
+ * found in delims parameter, or, if delims is NULL, any white space characters.
+ * @param str
+ * @param delims string containing delimiters. If NULL, white space characters
+ *      are used.
+ * @return trimmed string allocated with malloc or NULL if allocation failed
+ *      or given string was empty or contained only delimiters
+ */
+char *trim(const char *str, const char *delims);
+
+/*
+ * Check if the given vendor is like Dell. Some systems are OEMed with a
+ * different vendor name, even though the underlying h/w is from Dell.
+ * This function checks if the vendor is among those vendors.
+ * */
+bool is_vendor_like_dell(char *vendor);
+
+
+char * get_value_from_buffer(char *input, char **buffer, buffer_size);
+
+struct _Bmc_info{
+    char **IP4Addresses;
+    char **IP6Addresses;
+    int vlan;
+    char *PermanentMACAddress;
+    char **BMC_URLs;
+    char *FirmwareVersion;
+    char **supportedProtos;
+    char **supportedProtoVersions;
+    char **active_nic;
+}BMC_info;
